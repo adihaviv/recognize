204c21297914ef5f28b067c6040c975da9f7c7d6
Maxim Manco <maxim.manco@gmail.com>
2013-05-19 23:53:34 +0300

    stored facebook connection in session

--- recognize-web/src/main/java/org/kernelhost/recognize/web/command/UserSignInCommand.java
+++ recognize-web/src/main/java/org/kernelhost/recognize/web/command/UserSignInCommand.java
@@ -1,14 +1,11 @@
 package org.kernelhost.recognize.web.command;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.kernelhost.recognize.web.constant.Qualifier;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.web.SignInAdapter;
-import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.FacebookProfile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.NativeWebRequest;
 
@@ -16,15 +13,9 @@ import org.springframework.web.context.request.NativeWebRequest;
 public class UserSignInCommand implements SignInAdapter {
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public String signIn(String userId, Connection<?> connection, NativeWebRequest request) {
-		Connection<Facebook> facebookConnection = (Connection<Facebook>) connection;
-		Facebook facebook = facebookConnection.getApi();
-		
-		List<FacebookProfile> friendProfiles = facebook.friendOperations().getFriendProfiles();
-		byte[] userProfileImage = facebook.userOperations().getUserProfileImage(friendProfiles.get(0).getId());
-		
 		HttpSession session = request.getNativeRequest(HttpServletRequest.class).getSession();
+		session.setAttribute(Qualifier.USER_FACEBOOK_CONNECTION, connection);
 		
 		return null;
 	}
