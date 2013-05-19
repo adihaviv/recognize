652d1fc4617cb15ccace142d4cd0c72a0d74a212
Maxim Manco <maxim.manco@gmail.com>
2013-05-20 00:29:43 +0300

    added conncetion and generic user representation to HTTP session

--- recognize-web/src/main/java/org/kernelhost/recognize/web/command/UserSignInCommand.java
+++ recognize-web/src/main/java/org/kernelhost/recognize/web/command/UserSignInCommand.java
@@ -4,8 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.kernelhost.recognize.web.constant.Qualifier;
+import org.kernelhost.recognize.web.model.User;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.web.SignInAdapter;
+import org.springframework.social.facebook.api.Facebook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.NativeWebRequest;
 
@@ -15,7 +17,16 @@ public class UserSignInCommand implements SignInAdapter {
 	@Override
 	public String signIn(String userId, Connection<?> connection, NativeWebRequest request) {
 		HttpSession session = request.getNativeRequest(HttpServletRequest.class).getSession();
-		session.setAttribute(Qualifier.USER_FACEBOOK_CONNECTION, connection);
+		Object api = connection.getApi();
+		if (api instanceof Facebook) {
+			session.setAttribute(Qualifier.USER_FACEBOOK_CONNECTION, connection);
+		}
+		
+		User user = new User();
+		user.setId(connection.getKey().getProviderUserId());
+		user.setName(connection.getDisplayName());
+		
+		session.setAttribute(Qualifier.USER, user);
 		
 		return null;
 	}
