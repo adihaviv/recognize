ad406b835efbf039e68a47295f32a340039094fc
Maxim Manco <maxim.manco@gmail.com>
2013-05-20 00:29:53 +0300

    listed available app qualifiers

---
+++ recognize-web/src/main/java/org/kernelhost/recognize/web/constant/Qualifier.java
@@ -0,0 +1,8 @@
+package org.kernelhost.recognize.web.constant;
+
+public final class Qualifier {
+
+	public static final String USER = "USER";
+	public static final String USER_FACEBOOK_CONNECTION = "USER_FACEBOOK_CONNECTION";
+	
+}
